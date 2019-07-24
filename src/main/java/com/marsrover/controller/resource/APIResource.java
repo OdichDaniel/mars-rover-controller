9532fbd379de2a19872bead2950fad103719250b
Daniel <dev@xente.co>
2019-07-24 10:40:06 +0300

    Added APIResource class, configured web.xml

--- src/main/java/com/marsrover/controller/resource/APIResource.java
+++ src/main/java/com/marsrover/controller/resource/APIResource.java
@@ -1,5 +1,31 @@
 package com.marsrover.controller.resource;
 
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+
+@Path("/")
 public class APIResource {
 
+	
+
+		@POST
+		@Path("move")
+		@Consumes(MediaType.TEXT_PLAIN)
+		@Produces(MediaType.APPLICATION_JSON)
+		public Response receiveCommands(String commands) {
+
+			if (commands == null && commands.isEmpty()) {
+				return null;
+			}
+			return Response.ok().entity("Response").build();
+		}
+
+		
 }
